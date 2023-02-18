da1be53317620b77be75b866619eec1599ad4cad
JuanPabloFogliaco <juan.pablo.fogliaco@gmail.com>
2023-02-18 14:06:23 -0300

    Funcionalidad search

---
+++ src/componets/Search/index.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import { Search, WrapperSearch } from "./style";
+
+const SearchComponent = () => {
+
+  const [search, setSearch] = useState();
+
+  const onChangeSearch = (e:any) => {
+     setSearch(e.target.value) 
+  }
+
+  return (
+    <WrapperSearch>
+      <FiSearch style={{left: "45px", position: "relative"}} size={"30px"} color={"black"} />
+      <Search type={"text"} onChange={(e) => onChangeSearch(e)} placeholder="¿Que te apetece escuchar?" />
+    </WrapperSearch>
+  );
+};
+
+export default SearchComponent;
